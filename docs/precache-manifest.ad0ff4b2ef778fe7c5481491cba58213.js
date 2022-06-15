e29c1c18b321c1948149820426a3e52fa6974a32
João Pster <joaopsterdev@gmail.com>
2022-06-15 19:59:01 -0300

    Adicionado o site no GitHub Pages | #VQV :rocket:

---
+++ docs/precache-manifest.ad0ff4b2ef778fe7c5481491cba58213.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee96a2cff5504023f3daee2d6d6883d",
+    "url": "/Trivia-React-Redux/index.html"
+  },
+  {
+    "revision": "4f8ccad2da61e4a11e0f",
+    "url": "/Trivia-React-Redux/static/css/2.ba00281c.chunk.css"
+  },
+  {
+    "revision": "2645c18f2fb8a44181da",
+    "url": "/Trivia-React-Redux/static/css/main.83cdb4f7.chunk.css"
+  },
+  {
+    "revision": "4f8ccad2da61e4a11e0f",
+    "url": "/Trivia-React-Redux/static/js/2.2381d4e5.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/Trivia-React-Redux/static/js/2.2381d4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2645c18f2fb8a44181da",
+    "url": "/Trivia-React-Redux/static/js/main.13378b29.chunk.js"
+  },
+  {
+    "revision": "bffb6938de3700c265f4",
+    "url": "/Trivia-React-Redux/static/js/runtime-main.967f231e.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/Trivia-React-Redux/static/media/trivia.466d153e.png"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Store from './store/index';
 import App from './App';
@@ -9,9 +9,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ Store }>
-      <App />
-    </Provider>
+    <HashRouter basename="/">
+      <Provider store={ Store }>
+        <App />
+      </Provider>
+    </HashRouter>
   </BrowserRouter>,
 
   document.getElementById('root'),
